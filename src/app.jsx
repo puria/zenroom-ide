e58534afc4d28d33ba321223b5821ef05a92c94b
Puria Nafisi Azizi <puria@dyne.org>
2018-05-03 20:03:20 +0200

    :construction_worker: build script facilities for yarning best

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to Zenroom IDE!</h2>
+      <h1>Welcome to Zenroom IDE!</h1>
     </div>);
   }
 }
